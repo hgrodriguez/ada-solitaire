8d4db57ccce748ec60e18667ea32e46bbdad7ddc
Holger Rodriguez <holger.rodriguez@roseng.ch>
2019-07-06 15:15:11 +0200

    getting all_tests running

--- test/all_tests.adb
+++ test/all_tests.adb
@@ -1,4 +1,22 @@
+with AUnit.Test_Suites;
+with AUnit.Reporter;
+with AUnit.Reporter.Text;
+with AUnit.Run;
+
+with Deck.Test;
+
 procedure All_Tests is
+
+   function Suite return AUnit.Test_Suites.Access_Test_Suite is
+      Ret : constant AUnit.Test_Suites.Access_Test_Suite := new AUnit.Test_Suites.Test_Suite;
+   begin
+      Ret.Add_Test (Deck.Test.Suite);
+      return Ret;
+   end Suite;
+
+   procedure Runner is new AUnit.Run.Test_Runner (Suite);
+   Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
-   
+   Runner (Reporter);
+
 end All_Tests;

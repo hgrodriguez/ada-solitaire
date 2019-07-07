130af25f70978f7072de8ad3249a1bf68ee97b37
Holger Rodriguez <holger.rodriguez@roseng.ch>
2019-07-07 11:14:06 +0200

    only reformatted

--- test/deck_test_main.adb
+++ test/deck_test_main.adb
@@ -8,18 +8,16 @@ with Deck.Test;
 procedure Deck_Test_Main is
    
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
-      Ret : constant AUnit.Test_Suites.Access_Test_Suite := new AUnit.Test_Suites.Test_Suite;
+      Ret : constant AUnit.Test_Suites.Access_Test_Suite
+            := new AUnit.Test_Suites.Test_Suite;
    begin
       Ret.Add_Test (Deck.Test.Suite);
       return Ret;
    end Suite;
    
-   
-   
    procedure Runner is new AUnit.Run.Test_Runner (Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
+
 begin
    Runner (Reporter);
-
-   
 end Deck_Test_Main;

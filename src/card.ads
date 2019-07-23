05a7f590d4b6b04b973d3f340b7ec094c076744c
Holger Rodriguez <holger.rodriguez@roseng.ch>
2019-07-23 18:01:59 +0200

    adapted the source code to the new Ada style guide

--- src/card.ads
+++ src/card.ads
@@ -1,37 +1,38 @@
 with Deck;
 
--- Represents one card in a deck of (poker) cards
+--  Represents one card in a deck of (poker) cards
 package Card is
    type Card_Type is tagged private;
-   
-   -- Create one card
-   function Construct(Rank : in Deck.Rank_Type;
-                      Suit : in Deck.Suit_Type)
-                      return Card_Type;
 
-   -- Getters for attributes of a card
-   function Get_Rank (A_Card : in Card_Type) return Deck.Rank_Type;
-   function Get_Suit (A_Card : in Card_Type) return Deck.Suit_Type;
-   
-   -- rank comparison functions
-   function Rank_Is_Equal_To (c1 : in Card_Type; c2 : in Card_Type)
+   --  Create one card
+   function Construct (Rank : Deck.Rank_Type;
+                       Suit : Deck.Suit_Type)
+                       return Card_Type;
+
+   --  Getters for attributes of a card
+   function Get_Rank (A_Card : Card_Type) return Deck.Rank_Type;
+   function Get_Suit (A_Card : Card_Type) return Deck.Suit_Type;
+
+   --  rank comparison functions
+   function Rank_Is_Equal_To (c1 : Card_Type; c2 : Card_Type)
                               return Boolean;
-   -- semantics: c1.Rank > c2.Rank
-   function Rank_Is_Higher_Than (c1 : in Card_Type; c2 : in Card_Type)
+   --  semantics: c1.Rank > c2.Rank
+   function Rank_Is_Higher_Than (c1 : Card_Type; c2 : Card_Type)
                                  return Boolean;
-   -- semantics: c1.Rank < c2.Rank
-   function Rank_Is_Lower_Than (c1 : in Card_Type; c2 : in Card_Type)
+   --  semantics: c1.Rank < c2.Rank
+   function Rank_Is_Lower_Than (c1 : Card_Type; c2 : Card_Type)
                                 return Boolean;
-   
-   -- suit comparison functions
-   function Suit_Is_Equal_To (c1 : in Card_Type; c2 : in Card_Type)
-                              return Boolean;
-   function Suit_Is_Red (c: in Card_Type) return Boolean;
-   
-   function Is_Equal_To (c1 : in Card_Type; c2 : in Card_Type) return Boolean;
 
-   function Image (c : in Card_Type) return String;
-   
+   --  suit comparison functions
+   function Suit_Is_Equal_To (c1 : Card_Type; c2 : Card_Type)
+                              return Boolean;
+
+   function Suit_Is_Red (c : Card_Type) return Boolean;
+
+   function Is_Equal_To (c1 : Card_Type; c2 : Card_Type) return Boolean;
+
+   function Image (c : Card_Type) return String;
+
 private
    type Card_Type is tagged
       record
--- test/card_test_main.adb
+++ test/card_test_main.adb
@@ -6,7 +6,7 @@ with AUnit.Run;
 with Card.Test;
 
 procedure Card_Test_Main is
-   
+
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
       Ret : constant AUnit.Test_Suites.Access_Test_Suite
             := new AUnit.Test_Suites.Test_Suite;
@@ -14,7 +14,7 @@ procedure Card_Test_Main is
       Ret.Add_Test (Card.Test.Suite);
       return Ret;
    end Suite;
-   
+
    procedure Runner is new AUnit.Run.Test_Runner (Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 

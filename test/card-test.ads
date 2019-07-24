fd8f1b747a717a240992194d376e36ae33411836
Holger Rodriguez <holger.rodriguez@roseng.ch>
2019-07-24 18:32:53 +0200

    Card:  adapted the source code to the new Ada style guide

--- test/card-test.ads
+++ test/card-test.ads
@@ -1,7 +1,29 @@
 with AUnit.Test_Suites;
+with AUnit.Test_Fixtures;
 
 package Card.Test is
 
    function Suite return AUnit.Test_Suites.Access_Test_Suite;
+
+private
+   type Test is new AUnit.Test_Fixtures.Test_Fixture with null record;
+
+   procedure Ctor_Rank (T : in out Test);
+   procedure Ctor_Suit (T : in out Test);
+   procedure Rank_Is_Equal_To_Using_Identical_Rank (T : in out Test);
+   procedure Rank_Is_Equal_To_Using_Equal_Rank (T : in out Test);
+   procedure Rank_Is_Higher_Than_True (T : in out Test);
+   procedure Rank_Is_Higher_Than_False (T : in out Test);
+   procedure Rank_Is_Lower_Than_True (T : in out Test);
+   procedure Rank_Is_Lower_Than_False (T : in out Test);
+   procedure Suit_Is_Equal_To_Using_Identical_Suit (T : in out Test);
+   procedure Suit_Is_Equal_To_Using_Equal_Suit (T : in out Test);
+   procedure Suit_Is_Red_Diamond (T : in out Test);
+   procedure Suit_Is_Red_Heart (T : in out Test);
+   procedure Is_Equal_To_Identical_Rank_Identical_Suit (T : in out Test);
+   procedure Is_Equal_To_Equal_Rank_Identical_Suit (T : in out Test);
+   procedure Is_Equal_To_Identical_Rank_Equal_Suit (T : in out Test);
+   procedure Is_Equal_To_Equal_Rank_Equal_Suit (T : in out Test);
+   procedure Image_1 (T : in out Test);
 
 end Card.Test;
--- test/card_test_main.adb
+++ test/card_test_main.adb
@@ -7,6 +7,8 @@ with Card.Test;
 
 procedure Card_Test_Main is
 
+   function Suite return AUnit.Test_Suites.Access_Test_Suite;
+
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
       Ret : constant AUnit.Test_Suites.Access_Test_Suite
             := new AUnit.Test_Suites.Test_Suite;

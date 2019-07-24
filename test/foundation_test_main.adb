ba6a915c29b3d70447cc4d71cf0c8b566f8b0f45
Holger Rodriguez <holger.rodriguez@roseng.ch>
2019-07-24 18:39:45 +0200

    Foundation:  adapted the source code to the new Ada style guide

--- test/foundation_test_main.adb
+++ test/foundation_test_main.adb
@@ -6,7 +6,9 @@ with AUnit.Run;
 with Foundation.Test;
 
 procedure Foundation_Test_Main is
-   
+
+   function Suite return AUnit.Test_Suites.Access_Test_Suite;
+
    function Suite return AUnit.Test_Suites.Access_Test_Suite is
       Ret : constant AUnit.Test_Suites.Access_Test_Suite
             := new AUnit.Test_Suites.Test_Suite;
@@ -14,7 +16,7 @@ procedure Foundation_Test_Main is
       Ret.Add_Test (Foundation.Test.Suite);
       return Ret;
    end Suite;
-   
+
    procedure Runner is new AUnit.Run.Test_Runner (Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 

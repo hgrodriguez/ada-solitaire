3b50fa0c409cdb74bd85afb7b52920a805d5cff9
Holger Rodriguez <holger.rodriguez@roseng.ch>
2019-07-28 10:22:49 +0200

    Pile_Of_Cards.LIFO: implemented

---
+++ test/pile_of_cards_lifo_test_main.adb
@@ -0,0 +1,24 @@
+with AUnit.Test_Suites;
+with AUnit.Reporter;
+with AUnit.Reporter.Text;
+with AUnit.Run;
+
+with Pile_Of_Cards.LIFO.Test;
+
+procedure Pile_Of_Cards_LIFO_Test_Main is
+   function Suite return AUnit.Test_Suites.Access_Test_Suite;
+
+   function Suite return AUnit.Test_Suites.Access_Test_Suite is
+      Ret : constant AUnit.Test_Suites.Access_Test_Suite
+        := new AUnit.Test_Suites.Test_Suite;
+   begin
+      Ret.all.Add_Test (Pile_Of_Cards.LIFO.Test.Suite);
+      return Ret;
+   end Suite;
+
+   procedure Runner is new AUnit.Run.Test_Runner (Suite);
+   Reporter : AUnit.Reporter.Text.Text_Reporter;
+
+begin
+   Runner (Reporter);
+end Pile_Of_Cards_LIFO_Test_Main;

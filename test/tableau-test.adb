2816d25ce11a68746cade06ad2796c13bfce6396
Holger Rodriguez <holger.rodriguez@roseng.ch>
2019-08-03 11:59:27 +0200

    Tableau.Test: refactored code for adding cards and checking stack

--- test/tableau-test.adb
+++ test/tableau-test.adb
@@ -24,9 +24,9 @@ package body Tableau.Test is
                                  " /= 0");
    end Construct_Check_Size;
 
-   type Some_Cards is array (Natural range <>) of Card.Card_Type;
-   procedure Add_Cards_And_Check_Size (Cards : Some_Cards);
+   type Some_Cards is array (Positive range <>) of Card.Card_Type;
 
+   procedure Add_Cards_And_Check_Size (Cards : Some_Cards);
    procedure Add_Cards_And_Check_Size (Cards : Some_Cards) is
       Expected_Size : constant Natural := Cards'Last;
       Tab           : constant Tableau.Tableau_Type := Tableau.Construct;
@@ -75,79 +75,25 @@ package body Tableau.Test is
       Add_Cards_And_Check_Size (Cards);
    end Add_7_Cards_Check_Size;
 
-   procedure Add_1_Card_Check_Stack (T : in out Test) is
-      pragma Unreferenced (T);
+   procedure Add_Cards_And_Check_Stack (Cards : Some_Cards);
+   procedure Add_Cards_And_Check_Stack (Cards : Some_Cards) is
       Tab            : constant Tableau.Tableau_Type := Tableau.Construct;
       Pile           : Pile_Of_Cards.FIFO.Pile_Type_FIFO
         := Pile_Of_Cards.FIFO.Construct;
-      C_Push         : constant Card.Card_Type := Card.Construct (Deck.Six,
-                                                                  Deck.Diamond);
       Stack          : Tableau_Stack.Stack_Type_Access;
-      Expected_Sizes : constant array (Valid_Stacks_Range) of Natural
-        := (1, 0, 0, 0, 0, 0, 0);
+      Expected_Sizes : array (Valid_Stacks_Range) of Natural;
       Stack_Size     : Natural;
+      I              : Integer;
    begin
-      Pile.Put (C_Push);
-      Tab.Push (Pile);
       for J in Valid_Stacks_Range loop
-         Stack := Tab.Get_Stack (J);
-         Stack_Size := Stack.all.Size;
-         AUnit.Assertions.Assert (Stack_Size = Expected_Sizes (J),
-                                  "Size(" & J'Image & ")=" &
-                                    Expected_Sizes (J)'Image &
-                                    " /= " & Stack_Size'Image);
+         I := Integer (J);
+         if I > Cards'Last then
+            Expected_Sizes (J) := 0;
+         else
+            Expected_Sizes (J) := 1;
+         end if;
       end loop;
-   end Add_1_Card_Check_Stack;
-
-   procedure Add_3_Cards_Check_Stack (T : in out Test) is
-      pragma Unreferenced (T);
-      Tab            : constant Tableau.Tableau_Type := Tableau.Construct;
-      Pile           : Pile_Of_Cards.FIFO.Pile_Type_FIFO
-        := Pile_Of_Cards.FIFO.Construct;
-      C_Push1        : constant Card.Card_Type := Card.Construct (Deck.Six,
-                                                                  Deck.Diamond);
-      C_Push2        : constant Card.Card_Type := Card.Construct (Deck.Seven,
-                                                                  Deck.Club);
-      C_Push3        : constant Card.Card_Type := Card.Construct (Deck.King,
-                                                                  Deck.Heart);
-      Stack          : Tableau_Stack.Stack_Type_Access;
-      Expected_Sizes : constant array (Valid_Stacks_Range) of Natural
-        := (1, 1, 1, 0, 0, 0, 0);
-      Stack_Size     : Natural;
-   begin
-      Pile.Put (C_Push1);
-      Pile.Put (C_Push2);
-      Pile.Put (C_Push3);
-      Tab.Push (Pile);
-      for J in Valid_Stacks_Range loop
-         Stack := Tab.Get_Stack (J);
-         Stack_Size := Stack.all.Size;
-         AUnit.Assertions.Assert (Stack_Size = Expected_Sizes (J),
-                                  "Size(" & J'Image & ")=" &
-                                    Expected_Sizes (J)'Image &
-                                    " /= " & Stack_Size'Image);
-      end loop;
-   end Add_3_Cards_Check_Stack;
-
-   procedure Add_7_Cards_Check_Stack (T : in out Test) is
-      pragma Unreferenced (T);
-      Tab            : constant Tableau.Tableau_Type := Tableau.Construct;
-      Pile           : Pile_Of_Cards.FIFO.Pile_Type_FIFO
-        := Pile_Of_Cards.FIFO.Construct;
-      Cards          : constant array (1 .. 7) of Card.Card_Type
-        := (Card.Construct (Deck.Three, Deck.Diamond),
-            Card.Construct (Deck.Seven, Deck.Heart),
-            Card.Construct (Deck.Ace,   Deck.Spade),
-            Card.Construct (Deck.King,  Deck.Club),
-            Card.Construct (Deck.Eight, Deck.Diamond),
-            Card.Construct (Deck.Three, Deck.Spade),
-            Card.Construct (Deck.Jack,  Deck.Heart));
-      Stack          : Tableau_Stack.Stack_Type_Access;
-      Expected_Sizes : constant array (Valid_Stacks_Range) of Natural
-        := (1, 1, 1, 1, 1, 1, 1);
-      Stack_Size     : Natural;
-   begin
-      for J in Integer range 1 .. 7 loop
+      for J in Natural range Cards'First .. Cards'Last loop
          Pile.Put (Cards (J));
       end loop;
       Tab.Push (Pile);
@@ -159,6 +105,38 @@ package body Tableau.Test is
                                     Expected_Sizes (J)'Image &
                                     " /= " & Stack_Size'Image);
       end loop;
+   end Add_Cards_And_Check_Stack;
+
+   procedure Add_1_Card_Check_Stack (T : in out Test) is
+      pragma Unreferenced (T);
+      Cards          : constant Some_Cards
+        := (1 => Card.Construct (Deck.Three, Deck.Diamond));
+   begin
+      Add_Cards_And_Check_Stack (Cards);
+   end Add_1_Card_Check_Stack;
+
+   procedure Add_3_Cards_Check_Stack (T : in out Test) is
+      pragma Unreferenced (T);
+      Cards          : constant Some_Cards
+        := (Card.Construct (Deck.Three, Deck.Diamond),
+            Card.Construct (Deck.Three, Deck.Spade),
+            Card.Construct (Deck.Jack,  Deck.Heart));
+   begin
+      Add_Cards_And_Check_Stack (Cards);
+   end Add_3_Cards_Check_Stack;
+
+   procedure Add_7_Cards_Check_Stack (T : in out Test) is
+      pragma Unreferenced (T);
+      Cards          : constant Some_Cards
+        := (Card.Construct (Deck.Three, Deck.Diamond),
+            Card.Construct (Deck.Seven, Deck.Heart),
+            Card.Construct (Deck.Ace,   Deck.Spade),
+            Card.Construct (Deck.King,  Deck.Club),
+            Card.Construct (Deck.Eight, Deck.Diamond),
+            Card.Construct (Deck.Three, Deck.Spade),
+            Card.Construct (Deck.Jack,  Deck.Heart));
+   begin
+      Add_Cards_And_Check_Stack (Cards);
    end Add_7_Cards_Check_Stack;
 
    --------------------------------------------------------------------

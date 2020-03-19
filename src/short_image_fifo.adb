7a573348eb8862ed7058dc5d88aabc0b1c9666bf
Holger Rodriguez <holger.rodriguez@roseng.ch>
2020-03-19 15:44:59 +0100

    Short_Image_FIFO: only added comments and re-formatted

--- src/short_image_fifo.adb
+++ src/short_image_fifo.adb
@@ -1,27 +1,37 @@
 package body Short_Image_FIFO is
 
+   --------------------------------------------------------------------
+   --
    function Construct return Short_Image_FIFO_Type is
       SIF : Short_Image_FIFO_Type;
    begin
       return SIF;
    end Construct;
 
+   --------------------------------------------------------------------
+   --
    function Is_Empty (SIF : Short_Image_FIFO_Type) return Boolean is
    begin
       return SIF.Size = 0;
    end Is_Empty;
 
+   --------------------------------------------------------------------
+   --
    function Size (SIF : Short_Image_FIFO_Type) return Natural is
    begin
       return Natural (Short_Image_DLL.Length (SIF.SIDLL));
    end Size;
 
+   --------------------------------------------------------------------
+   --
    procedure Put (SIF : in out Short_Image_FIFO_Type;
                   CSI : Card.Short_Image_Type) is
    begin
       SIF.SIDLL.Append (CSI);
    end Put;
 
+   --------------------------------------------------------------------
+   --
    function Get (SIF : in out Short_Image_FIFO_Type)
                  return Card.Short_Image_Type is
       ret_val : Card.Short_Image_Type;
--- src/short_image_fifo.ads
+++ src/short_image_fifo.ads
@@ -1,3 +1,4 @@
+--  helper for collecting Card.Short_Image_Types for printing
 with Ada.Containers;
 with Ada.Containers.Doubly_Linked_Lists;
 
@@ -8,14 +9,26 @@ package Short_Image_FIFO is
    type Short_Image_FIFO_Type is tagged private;
    type Short_Image_FIFO_Type_Access is access all Short_Image_FIFO_Type;
 
-   Short_Image_FIFO_Empty_Exception : exception;
-
+   --------------------------------------------------------------------
+   --  construct a short image fifo
    function Construct return Short_Image_FIFO_Type;
 
+   --------------------------------------------------------------------
+   --  check, if fifo is empty
    function Is_Empty (SIF : Short_Image_FIFO_Type) return Boolean;
+
+   --------------------------------------------------------------------
+   --  return size/depth of fifo
    function Size (SIF : Short_Image_FIFO_Type) return Natural;
+
+   --------------------------------------------------------------------
+   --  put one short image into the fifo
    procedure Put (SIF : in out Short_Image_FIFO_Type;
                   CSI : Card.Short_Image_Type);
+   --------------------------------------------------------------------
+   --  get one short image into the fifo
+   --  if fifo is empty -> exception
+   Short_Image_FIFO_Empty_Exception : exception;
    function Get (SIF : in out Short_Image_FIFO_Type)
                  return Card.Short_Image_Type;
 
